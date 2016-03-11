d6eb0f3f562cf06dbf61c7fa4a372746e90156a5
Dejan Lavbič <Dejan@lavbic.net>
2016-03-11 12:12:53 +0100

    Implementacija logike na strani odjemalca.

---
+++ public/js/klepet.js
@@ -0,0 +1,41 @@
+var Klepet = function(socket) {
+  this.socket = socket;
+};
+
+Klepet.prototype.posljiSporocilo = function(kanal, besedilo) {
+  var sporocilo = {
+    kanal: kanal,
+    besedilo: besedilo
+  };
+  this.socket.emit('sporocilo', sporocilo);
+};
+
+Klepet.prototype.spremeniKanal = function(kanal) {
+  this.socket.emit('pridruzitevZahteva', {
+    novKanal: kanal
+  });
+};
+
+Klepet.prototype.procesirajUkaz = function(ukaz) {
+  var besede = ukaz.split(' ');
+  ukaz = besede[0].substring(1, besede[0].length).toLowerCase();
+  var sporocilo = false;
+
+  switch(ukaz) {
+    case 'pridruzitev':
+      besede.shift();
+      var kanal = besede.join(' ');
+      this.spremeniKanal(kanal);
+      break;
+    case 'vzdevek':
+      besede.shift();
+      var vzdevek = besede.join(' ');
+      this.socket.emit('vzdevekSpremembaZahteva', vzdevek);
+      break;
+    default:
+      sporocilo = 'Neznan ukaz.';
+      break;
+  };
+
+  return sporocilo;
+};
---
+++ public/js/klepet_UI.js
@@ -0,0 +1,78 @@
+function divElementEnostavniTekst(sporocilo) {
+  return $('<div style="font-weight: bold"></div>').text(sporocilo);
+}
+
+function divElementHtmlTekst(sporocilo) {
+  return $('<div></div>').html('<i>' + sporocilo + '</i>');
+}
+
+function procesirajVnosUporabnika(klepetApp, socket) {
+  var sporocilo = $('#poslji-sporocilo').val();
+  var sistemskoSporocilo;
+
+  if (sporocilo.charAt(0) == '/') {
+    sistemskoSporocilo = klepetApp.procesirajUkaz(sporocilo);
+    if (sistemskoSporocilo) {
+      $('#sporocila').append(divElementHtmlTekst(sistemskoSporocilo));
+    }
+  } else {
+    klepetApp.posljiSporocilo($('#kanal').text(), sporocilo);
+    $('#sporocila').append(divElementEnostavniTekst(sporocilo));
+    $('#sporocila').scrollTop($('#sporocila').prop('scrollHeight'));
+  }
+
+  $('#poslji-sporocilo').val('');
+}
+
+var socket = io.connect();
+
+$(document).ready(function() {
+  var klepetApp = new Klepet(socket);
+
+  socket.on('vzdevekSpremembaOdgovor', function(rezultat) {
+    var sporocilo;
+    if (rezultat.uspesno) {
+      sporocilo = 'Prijavljen si kot ' + rezultat.vzdevek + '.';
+    } else {
+      sporocilo = rezultat.sporocilo;
+    }
+    $('#sporocila').append(divElementHtmlTekst(sporocilo));
+  });
+
+  socket.on('pridruzitevOdgovor', function(rezultat) {
+    $('#kanal').text(rezultat.kanal);
+    $('#sporocila').append(divElementHtmlTekst('Sprememba kanala.'));
+  });
+
+  socket.on('sporocilo', function (sporocilo) {
+    var novElement = $('<div style="font-weight: bold"></div>').text(sporocilo.besedilo);
+    $('#sporocila').append(novElement);
+  });
+
+  socket.on('kanali', function(kanali) {
+    $('#seznam-kanalov').empty();
+
+    for(var kanal in kanali) {
+      kanal = kanal.substring(1, kanal.length);
+      if (kanal != '') {
+        $('#seznam-kanalov').append(divElementEnostavniTekst(kanal));
+      }
+    }
+
+    $('#seznam-kanalov div').click(function() {
+      klepetApp.procesirajUkaz('/pridruzitev ' + $(this).text());
+      $('#poslji-sporocilo').focus();
+    });
+  });
+
+  setInterval(function() {
+    socket.emit('kanali');
+  }, 1000);
+
+  $('#poslji-sporocilo').focus();
+
+  $('#poslji-obrazec').submit(function() {
+    procesirajVnosUporabnika(klepetApp, socket);
+    return false;
+  });
+});
